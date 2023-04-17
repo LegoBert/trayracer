069fde60062b85d7b76d7e94862ecc6c85f09ad4
Kit Sundström <93656571+LegoBert@users.noreply.github.com>
2023-04-17 19:25:29 +0200

    2.1&2

--- main.cc
+++ main.cc
@@ -3,9 +3,201 @@
 #include "vec3.h"
 #include "raytracer.h"
 #include "sphere.h"
+#include <iostream>
+#include <chrono>
 
 #define degtorad(angle) angle * MPI / 180
 
+#define no_gl 1
+#if no_gl
+int main(int argc, char* argv[])
+{
+    if (argc != 5) {
+        std::cout << "Usage: " << argv[0] << " <width> <height> <raysPerPixel> <numOfSpheres>" << std::endl;
+        return 1;
+    }
+    int w = atoi(argv[1]);
+    int h = atoi(argv[2]);
+    int raysPerPixel = atoi(argv[3]);
+    int maxBounces = 5;
+    int numOfSpheres = atoi(argv[4]);
+
+    Display::Window wnd;
+
+    wnd.SetTitle("TrayRacer");
+
+    if (!wnd.Open())
+        return 1;
+
+    std::vector<Color> framebuffer;
+
+    framebuffer.resize(w * h);
+
+    Raytracer rt = Raytracer(w, h, framebuffer, raysPerPixel, maxBounces);
+
+    // Create some objects
+    Material* mat = new Material();
+    mat->type = "Lambertian";
+    mat->color = { 0.5,0.5,0.5 };
+    mat->roughness = 0.3;
+    Sphere* ground = new Sphere(1000, { 0,-1000, -1 }, mat);
+    rt.AddObject(ground);
+
+    //Creating spheres
+    for (int i = 0; i < numOfSpheres; i++)
+    {
+        Material* mat = new Material();
+        mat->type = "Lambertian";
+        float r = RandomFloat();
+        float g = RandomFloat();
+        float b = RandomFloat();
+        mat->color = { r,g,b };
+        mat->roughness = RandomFloat();
+        const float span = 10.0f;
+        Sphere* ground = new Sphere(
+            RandomFloat() * 0.7f + 0.2f,
+            {
+                RandomFloatNTP() * span,
+                RandomFloat() * span + 0.2f,
+                RandomFloatNTP() * span
+            },
+            mat);
+        rt.AddObject(ground);
+    }
+
+    bool exit = false;
+
+    // camera
+    bool resetFramebuffer = false;
+    vec3 camPos = { 0,1.0f,10.0f };
+    vec3 moveDir = { 0,0,0 };
+
+    wnd.SetKeyPressFunction([&exit, &moveDir, &resetFramebuffer](int key, int scancode, int action, int mods)
+        {
+            switch (key)
+            {
+            case GLFW_KEY_ESCAPE:
+                exit = true;
+                break;
+            case GLFW_KEY_W:
+                moveDir.z -= 1.0f;
+                resetFramebuffer |= true;
+                break;
+            case GLFW_KEY_S:
+                moveDir.z += 1.0f;
+                resetFramebuffer |= true;
+                break;
+            case GLFW_KEY_A:
+                moveDir.x -= 1.0f;
+                resetFramebuffer |= true;
+                break;
+            case GLFW_KEY_D:
+                moveDir.x += 1.0f;
+                resetFramebuffer |= true;
+                break;
+            case GLFW_KEY_SPACE:
+                moveDir.y += 1.0f;
+                resetFramebuffer |= true;
+                break;
+            case GLFW_KEY_LEFT_CONTROL:
+                moveDir.y -= 1.0f;
+                resetFramebuffer |= true;
+                break;
+            default:
+                break;
+            }
+        });
+
+    float pitch = 0;
+    float yaw = 0;
+    float oldx = 0;
+    float oldy = 0;
+
+    wnd.SetMouseMoveFunction([&pitch, &yaw, &oldx, &oldy, &resetFramebuffer](double x, double y)
+        {
+            x *= -0.1;
+    y *= -0.1;
+    yaw = x - oldx;
+    pitch = y - oldy;
+    resetFramebuffer |= true;
+    oldx = x;
+    oldy = y;
+        });
+
+    float rotx = 0;
+    float roty = 0;
+
+    // number of accumulated frames
+    int frameIndex = 0;
+
+    std::vector<Color> framebufferCopy;
+    framebufferCopy.resize(w * h);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1; i++) {
+        resetFramebuffer = false;
+        moveDir = { 0,0,0 };
+        pitch = 0;
+        yaw = 0;
+
+        // poll input
+        wnd.Update();
+
+        rotx -= pitch;
+        roty -= yaw;
+
+        moveDir = normalize(moveDir);
+
+        mat4 xMat = (rotationx(rotx));
+        mat4 yMat = (rotationy(roty));
+        mat4 cameraTransform = multiply(yMat, xMat);
+
+        camPos = camPos + transform(moveDir * 0.2f, cameraTransform);
+
+        cameraTransform.m30 = camPos.x;
+        cameraTransform.m31 = camPos.y;
+        cameraTransform.m32 = camPos.z;
+
+        rt.SetViewMatrix(cameraTransform);
+
+        if (resetFramebuffer)
+        {
+            rt.Clear();
+            frameIndex = 0;
+        }
+
+        rt.Raytrace();
+        frameIndex++;
+
+        // Get the average distribution of all samples
+        {
+            size_t p = 0;
+            for (Color const& pixel : framebuffer)
+            {
+                framebufferCopy[p] = pixel;
+                framebufferCopy[p].r /= frameIndex;
+                framebufferCopy[p].g /= frameIndex;
+                framebufferCopy[p].b /= frameIndex;
+                p++;
+            }
+        }
+
+        glClearColor(0, 0, 0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        wnd.Blit((float*)&framebufferCopy[0], w, h);
+        wnd.SwapBuffers();
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Time taken by code: " << duration.count() << " microseconds" << std::endl;
+
+    if (wnd.IsOpen())
+        wnd.Close();
+
+    return 0;
+}
+#else
 int main()
 { 
     Display::Window wnd;
@@ -223,4 +415,5 @@ int main()
         wnd.Close();
 
     return 0;
-} 
+}
+#endif
